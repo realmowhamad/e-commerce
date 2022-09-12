fcb7037ce33560e8231a926cd7c8bba1cc1ee67c
Mohammad Mirhoseini <Rm.mirhoseini@gmail.com>
2022-09-12 23:12:56 +0430

    Bottom Bar UI

--- src/Components/BottomBar/BottomBar.js
+++ src/Components/BottomBar/BottomBar.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import './BottomBar.css'
+import { BsList } from "react-icons/bs"
+import { RiHomeLine } from "react-icons/ri"
+import { CgShoppingBag } from "react-icons/cg"
+import { AiOutlineHeart, AiOutlineUser } from "react-icons/ai"
 
 export default function BottomBar() {
     return (
-        <>
-            <Container>
-                        
+        <div className="bottomBar-icons">
+            <NavLink to="/" className="Home">{<RiHomeLine />}</NavLink>
+            <NavLink to="/" className="Home">{<AiOutlineHeart />}</NavLink>
+            <NavLink to="/" className="list">{<BsList />}</NavLink>
+            <NavLink to="/" className="Home">{<CgShoppingBag />}</NavLink>
+            <NavLink to="/" className="Home">{<AiOutlineUser />}</NavLink>
+        </div>
 
 
-            </Container>
-        </>
     )
 }
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import BottomBar from '../BottomBar/BottomBar';
 import { Container, Row, Col } from 'react-bootstrap'
-
 import './Main.css'
 
 
